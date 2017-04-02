b4fcce12a65940e271d012358ce1d7f09ea88d76
CaptainBlagbird <CaptainBlagbird@mailueberfall.de>
2017-04-02 17:30:46 +0200

    Tabs --> Spaces

--- AutoHandleSharedQuests/AutoHandleSharedQuests.lua
+++ AutoHandleSharedQuests/AutoHandleSharedQuests.lua
@@ -10,45 +10,45 @@ https://github.com/CaptainBlagbird
 local AddonName = "AutoHandleSharedQuests"
 AutoHandleSharedQuests = {}
 AutoHandleSharedQuests.SavedVarsDefault = {
-	ActionAvA = GetString(SI_AHSQ_ACTION_NONE),
-	ActionPvE = GetString(SI_AHSQ_ACTION_NONE)
+    ActionAvA = GetString(SI_AHSQ_ACTION_NONE),
+    ActionPvE = GetString(SI_AHSQ_ACTION_NONE)
 }
 
 -- Local variables
 local doAction = {
-	[GetString(SI_AHSQ_ACTION_NONE)] = function(questId)
-			-- Nothing
-		end,
-	[GetString(SI_AHSQ_ACTION_ACCEPT)] = function(questId)
-			AcceptSharedQuest(questId)
-			d(zo_strformat(GetString(SI_AHSQ_MSG_ACCEPTED), GetOfferedQuestShareInfo(questId)))
-		end,
-	[GetString(SI_AHSQ_ACTION_DECLINE)] = function(questId)
-			DeclineSharedQuest(questId)
-			d(zo_strformat(GetString(SI_AHSQ_MSG_DECLINED), GetOfferedQuestShareInfo(questId)))
-		end,
+    [GetString(SI_AHSQ_ACTION_NONE)] = function(questId)
+            -- Nothing
+        end,
+    [GetString(SI_AHSQ_ACTION_ACCEPT)] = function(questId)
+            AcceptSharedQuest(questId)
+            d(zo_strformat(GetString(SI_AHSQ_MSG_ACCEPTED), GetOfferedQuestShareInfo(questId)))
+        end,
+    [GetString(SI_AHSQ_ACTION_DECLINE)] = function(questId)
+            DeclineSharedQuest(questId)
+            d(zo_strformat(GetString(SI_AHSQ_MSG_DECLINED), GetOfferedQuestShareInfo(questId)))
+        end,
 }
 
 
 -- Event handler function for EVENT_QUEST_SHARED
 local function OnQuestShared(eventCode, questId)
-	-- GetCompletedQuestInfo(questId)
-	local ava = AutoHandleSharedQuests.SavedVars.ActionAvA
-	local pve = AutoHandleSharedQuests.SavedVars.ActionPvE
-	if IsPlayerInAvAWorld() then
-		doAction[ava](questId)
-	else
-		doAction[pve](questId)
-	end
+    -- GetCompletedQuestInfo(questId)
+    local ava = AutoHandleSharedQuests.SavedVars.ActionAvA
+    local pve = AutoHandleSharedQuests.SavedVars.ActionPvE
+    if IsPlayerInAvAWorld() then
+        doAction[ava](questId)
+    else
+        doAction[pve](questId)
+    end
 end
 
 -- Event handler function for EVENT_PLAYER_ACTIVATED
 local function OnPlayerActivated(eventCode)
-	-- Set up SavedVariables table
-	AutoHandleSharedQuests.SavedVars = ZO_SavedVars:New(AddonName.."_SavedVars", 1, nil, AutoHandleSharedQuests.SavedVarsDefault)
-	-- Register events that use SavedVariables
-	EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_SHARED, OnQuestShared)
-	-- Unregister this event
-	EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_PLAYER_ACTIVATED)
+    -- Set up SavedVariables table
+    AutoHandleSharedQuests.SavedVars = ZO_SavedVars:New(AddonName.."_SavedVars", 1, nil, AutoHandleSharedQuests.SavedVarsDefault)
+    -- Register events that use SavedVariables
+    EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_SHARED, OnQuestShared)
+    -- Unregister this event
+    EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_PLAYER_ACTIVATED)
 end
 EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
--- AutoHandleSharedQuests/Settings.lua
+++ AutoHandleSharedQuests/Settings.lua
@@ -9,43 +9,43 @@ https://github.com/CaptainBlagbird
 local AddonName = "AutoHandleSharedQuests"
 
 local panelData = {
-	type = "panel",
-	name = "Auto Handle Shared Quests",
-	displayName = "|c70C0DEAuto Handle Shared Quests|r",
-	author = "|c70C0DECaptainBlagbird|r",
-	version = "1.1",
-	registerForRefresh = true,
-	registerForDefaults = true,
+    type = "panel",
+    name = "Auto Handle Shared Quests",
+    displayName = "|c70C0DEAuto Handle Shared Quests|r",
+    author = "|c70C0DECaptainBlagbird|r",
+    version = "1.1",
+    registerForRefresh = true,
+    registerForDefaults = true,
 }
 
 local optionsTable = {
-	{
-		type = "dropdown",
-		name = GetString(SI_AHSQ_DESC_AVA),
-		tooltip = GetString(SI_AHSQ_TT_AVA),
-		choices = {GetString(SI_AHSQ_ACTION_NONE), GetString(SI_AHSQ_ACTION_ACCEPT), GetString(SI_AHSQ_ACTION_DECLINE)},
-		getFunc = function() return AutoHandleSharedQuests.SavedVars.ActionAvA end,
-		setFunc = function(str) AutoHandleSharedQuests.SavedVars.ActionAvA = str end,
-		default = AutoHandleSharedQuests.SavedVarsDefault.ActionAvA,
-	},
-	{
-		type = "dropdown",
-		name = GetString(SI_AHSQ_DESC_PVE),
-		tooltip = GetString(SI_AHSQ_TT_PVE),
-		choices = {GetString(SI_AHSQ_ACTION_NONE), GetString(SI_AHSQ_ACTION_ACCEPT), GetString(SI_AHSQ_ACTION_DECLINE)},
-		getFunc = function() return AutoHandleSharedQuests.SavedVars.ActionPvE end,
-		setFunc = function(str) AutoHandleSharedQuests.SavedVars.ActionPvE = str end,
-		default = AutoHandleSharedQuests.SavedVarsDefault.ActionPvE,
-	},
+    {
+        type = "dropdown",
+        name = GetString(SI_AHSQ_DESC_AVA),
+        tooltip = GetString(SI_AHSQ_TT_AVA),
+        choices = {GetString(SI_AHSQ_ACTION_NONE), GetString(SI_AHSQ_ACTION_ACCEPT), GetString(SI_AHSQ_ACTION_DECLINE)},
+        getFunc = function() return AutoHandleSharedQuests.SavedVars.ActionAvA end,
+        setFunc = function(str) AutoHandleSharedQuests.SavedVars.ActionAvA = str end,
+        default = AutoHandleSharedQuests.SavedVarsDefault.ActionAvA,
+    },
+    {
+        type = "dropdown",
+        name = GetString(SI_AHSQ_DESC_PVE),
+        tooltip = GetString(SI_AHSQ_TT_PVE),
+        choices = {GetString(SI_AHSQ_ACTION_NONE), GetString(SI_AHSQ_ACTION_ACCEPT), GetString(SI_AHSQ_ACTION_DECLINE)},
+        getFunc = function() return AutoHandleSharedQuests.SavedVars.ActionPvE end,
+        setFunc = function(str) AutoHandleSharedQuests.SavedVars.ActionPvE = str end,
+        default = AutoHandleSharedQuests.SavedVarsDefault.ActionPvE,
+    },
 }
 
 -- Wait until all addons are loaded
 local function OnPlayerActivated(eventCode)
-	EVENT_MANAGER:UnregisterForEvent(AddonName.."_Settings", EVENT_PLAYER_ACTIVATED)
-	if LibStub ~= nil then
-		local LAM = LibStub("LibAddonMenu-2.0")
-		LAM:RegisterAddonPanel(AddonName.."_Options", panelData)
-		LAM:RegisterOptionControls(AddonName.."_Options", optionsTable)
-	end
+    EVENT_MANAGER:UnregisterForEvent(AddonName.."_Settings", EVENT_PLAYER_ACTIVATED)
+    if LibStub ~= nil then
+        local LAM = LibStub("LibAddonMenu-2.0")
+        LAM:RegisterAddonPanel(AddonName.."_Options", panelData)
+        LAM:RegisterOptionControls(AddonName.."_Options", optionsTable)
+    end
 end
 EVENT_MANAGER:RegisterForEvent(AddonName.."_Settings", EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
